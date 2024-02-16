46de262a0f26178bb684f4934af118f3e8dffdc5
Jongyoul Lee <jongyoul@gmail.com>
2024-02-16 13:12:24 +0900

    [HOTFIX] Check permission when updating cron information (#4631)
    * [HOTFIX] Check permission when updating cron information
    
    * [HOTFIX] Fix commented
    
    * [HOTFIX] Check permission when updating cron information
    
    * [HOTFIX] Check permission when updating cron information
    
    * [HOTFIX] Check permission when updating cron information

--- zeppelin-zengine/src/test/java/org/apache/zeppelin/notebook/NotebookTest.java
+++ zeppelin-zengine/src/test/java/org/apache/zeppelin/notebook/NotebookTest.java
@@ -53,6 +53,7 @@ import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -106,6 +107,9 @@ public class NotebookTest extends AbstractInterpreterTest implements ParagraphJo
     schedulerService = new QuartzSchedulerService(conf, notebook);
     notebook.initNotebook();
     notebook.waitForFinishInit(1, TimeUnit.MINUTES);
+
+    // create empty shiro.ini file under confDir
+    Files.createFile(new File(confDir, "shiro.ini").toPath());
   }
 
   @Override
