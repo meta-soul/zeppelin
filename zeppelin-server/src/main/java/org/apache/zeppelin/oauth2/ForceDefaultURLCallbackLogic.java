ca0bb0e7c3556ab87420ee16c17be31bfb9d0256
Asakiny <asakiny@dmetasoul.com>
2024-04-19 18:16:51 +0800

    bugfix zeppelin callback logic

--- zeppelin-server/src/main/java/org/apache/zeppelin/oauth2/ForceDefaultURLCallbackLogic.java
+++ zeppelin-server/src/main/java/org/apache/zeppelin/oauth2/ForceDefaultURLCallbackLogic.java
@@ -1,26 +1,49 @@
 package org.apache.zeppelin.oauth2;
 
+import org.pac4j.core.context.HttpConstants;
 import org.pac4j.core.context.WebContext;
-import org.pac4j.core.context.session.SessionStore;
 import org.pac4j.core.engine.DefaultCallbackLogic;
 import org.pac4j.core.exception.http.HttpAction;
-import org.pac4j.core.util.HttpActionHelper;
+import org.pac4j.core.exception.http.RedirectionAction;
+import org.pac4j.core.exception.http.SeeOtherAction;
+
 import org.pac4j.core.util.Pac4jConstants;
 import org.pac4j.core.exception.http.FoundAction;
 import org.pac4j.core.util.CommonHelper;
 
 public class ForceDefaultURLCallbackLogic extends DefaultCallbackLogic {
+    private static boolean useModernHttpCodes = true;
+
+    private static boolean alwaysUse401ForUnauthenticated = true;
 
     @Override
-    public HttpAction redirectToOriginallyRequestedUrl(final WebContext context, final SessionStore sessionStore,
+    public HttpAction redirectToOriginallyRequestedUrl(final WebContext context,
                                                           final String defaultUrl) {
         if(CommonHelper.isNotBlank(defaultUrl) && 
             !Pac4jConstants.DEFAULT_URL_VALUE.equals(defaultUrl)) {
-            return HttpActionHelper.buildRedirectUrlAction(context, ( new FoundAction(defaultUrl) ).getLocation());
+            return buildRedirectUrlAction(context, ( new FoundAction(defaultUrl) ).getLocation());
         } else {
-            return super.redirectToOriginallyRequestedUrl(context, sessionStore, "");
+            return super.redirectToOriginallyRequestedUrl(context, "");
         }
     }
 
+    /**
+     * Build the appropriate redirection action for a location.
+     *
+     * @param context the web context
+     * @param location the location
+     * @return the appropriate redirection action
+     */
+    public static RedirectionAction buildRedirectUrlAction(final WebContext context, final String location) {
+        if (isPost(context) && useModernHttpCodes) {
+            return new SeeOtherAction(location);
+        } else {
+            return new FoundAction(location);
+        }
+    }
+
+    public static boolean isPost(final WebContext context) {
+        return HttpConstants.HTTP_METHOD.POST.name().equalsIgnoreCase(context.getRequestMethod());
+    }
     
 }
