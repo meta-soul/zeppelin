f4af713c4ef440fb01c4dc8a44faf32e46a19688
Asakiny <asakiny@dmetasoul.com>
2024-08-13 17:43:18 +0800

    fix: cve security update

--- scripts/docker/zeppelin/bin/build-base.sh
+++ scripts/docker/zeppelin/bin/build-base.sh
@@ -1,1 +1,1 @@
-docker build -t swr.cn-southwest-2.myhuaweicloud.com/dmetasoul-repo/zeppelin-base:v0.0.4 -f Dockerfile-Base .
+docker build -t swr.cn-southwest-2.myhuaweicloud.com/dmetasoul-repo/zeppelin-base:v0.0.5 -f Dockerfile-Base .
